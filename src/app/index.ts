ac63e409de672064f2a177565f288aecbf50b326
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2021-01-04 12:06:06 +0700

    Prettier

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,7 @@
 import "../../scss/base.scss";
 import { party } from "./party";
 
-party([[255,255,0], [0,255,255]]);
-
+party([
+    [255, 255, 0],
+    [0, 255, 255],
+]);
--- src/app/party.ts
+++ src/app/party.ts
@@ -7,10 +7,10 @@ function party(colors: Colors): void {
         .getUserMedia({
             // eslint-disable-next-line
             // @ts-ignore
-            audio:{mandatory:{chromeMediaSource:'desktop'}}, 
+            audio: { mandatory: { chromeMediaSource: "desktop" } },
             // eslint-disable-next-line
             // @ts-ignore
-            video:{mandatory:{chromeMediaSource:'desktop'}}
+            video: { mandatory: { chromeMediaSource: "desktop" } },
         })
         .then((stream) => {
             const audioContext = new AudioContext();
@@ -28,7 +28,11 @@ function party(colors: Colors): void {
         .catch(console.error);
 }
 
-function calculate(audioContext: AudioContext, analyser: AnalyserNode, colors: Colors): void {
+function calculate(
+    audioContext: AudioContext,
+    analyser: AnalyserNode,
+    colors: Colors
+): void {
     requestAnimationFrame(() => calculate(audioContext, analyser, colors));
 
     const volume = get_volume(analyser, 50, 3);
@@ -39,18 +43,21 @@ function calculate(audioContext: AudioContext, analyser: AnalyserNode, colors: C
     pitch = pitch > pitch_max ? pitch_max : pitch;
     pitch = pitch < pitch_min ? pitch_min : pitch;
 
-    pitch = (pitch - pitch_min) * 100 / (pitch_max - pitch_min);    
+    pitch = ((pitch - pitch_min) * 100) / (pitch_max - pitch_min);
 
     const color = get_color(colors, pitch);
 
-    $("body").css("background-color", `rgb(${color.map(x => x * volume / 100).join(",")})`);
+    $("body").css(
+        "background-color",
+        `rgb(${color.map((x) => (x * volume) / 100).join(",")})`
+    );
 }
 
-type Color = [number, number, number]
+type Color = [number, number, number];
 type Colors = Color[];
 function get_color(colors: Colors, pos: number): Color {
-    if (!colors.length)return [0,0,0];
-    else if (colors.length == 1)return colors[0];
+    if (!colors.length) return [0, 0, 0];
+    else if (colors.length == 1) return colors[0];
 
     // Limit range to 0-100.
     let limit_pos = pos < 0 ? 0 : pos;
@@ -59,20 +66,20 @@ function get_color(colors: Colors, pos: number): Color {
     // Get color positions
     const positions: number[] = [];
     colors.forEach((val, i) => {
-        positions.push(i / (colors.length - 1) * 100);
+        positions.push((i / (colors.length - 1)) * 100);
     });
 
     // If the position only overlaps 1 of the colors
     if (positions.includes(limit_pos)) {
         return colors[positions.indexOf(limit_pos)];
     } else {
-        let color_low: Color = [0,0,0];
-        let color_high: Color = [0,0,0];
-        
+        let color_low: Color = [0, 0, 0];
+        let color_high: Color = [0, 0, 0];
+
         let pos_low = 0;
         let pos_high = 0;
 
-        for (let i = 0;i < positions.length;i++) {
+        for (let i = 0; i < positions.length; i++) {
             const position = positions[i];
             if (position > limit_pos) {
                 pos_low = positions[i - 1];
@@ -85,20 +92,28 @@ function get_color(colors: Colors, pos: number): Color {
             }
         }
 
-        let diff = (limit_pos - pos_low - (100 - pos_high)) / (pos_high - pos_low) * 100;
+        let diff =
+            ((limit_pos - pos_low - (100 - pos_high)) / (pos_high - pos_low)) *
+            100;
 
-        if (diff < 0)diff += 100;
+        if (diff < 0) diff += 100;
 
-        const final_color: Color = [0,0,0];
-        for (let j = 0;j < 3;j++) {
-            final_color[j] = (color_high[j] * diff / 100) + (color_low[j] * (100 - diff) / 100);
+        const final_color: Color = [0, 0, 0];
+        for (let j = 0; j < 3; j++) {
+            final_color[j] =
+                (color_high[j] * diff) / 100 +
+                (color_low[j] * (100 - diff)) / 100;
         }
 
         return final_color;
     }
 }
 
-function get_volume(analyser: AnalyserNode, volume_ceil: number, multiplier?: number): number {
+function get_volume(
+    analyser: AnalyserNode,
+    volume_ceil: number,
+    multiplier?: number
+): number {
     const volume_data_arr = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(volume_data_arr);
 
